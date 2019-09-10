885b65bf52df65ed5d30c7756db9f0b65f655068
Mani7499 <39701816+Mani7499@users.noreply.github.com>
2019-09-10 16:43:21 +0530

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 var express=require("express"); 
+
 var bodyParser=require("body-parser"); 
 const path=require('path')
+const ejs=require('ejs')
 const fs=require('fs')
 const user=require('./src/models/users.js')
 const team=require('./src/models/team.js')
 var us;
 const sendmail=require('./utilities/accounte.js')
-
-
+var ussr;
 var app=express() 
-//app.set('view engine','ejs');
+app.set('view engine','ejs');
 const dirPath=path.join(__dirname,'/src')
 app.use(bodyParser.json()); 
 app.use(express.static(dirPath)); 
+
 app.use(bodyParser.urlencoded({ 
 	extended: true
 })); 
@@ -40,12 +42,12 @@ app.post('/sign_up', (req,res)=>{
              uorg: '#',
              taskcom: 0,
              tasktodo: '.',
-             utype: req.body.utype
+             utype: "user"
     })
       
      uss.save().then(()=>{
         sendmail.sendmail(uss.uemail,uss.uname)
-       return res.redirect('login')
+       return res.redirect('login.html')
     }).catch((error)=>{
           return res.send('invalid user detail or internet connection is not there')
     })
@@ -55,18 +57,31 @@ app.post('/sign_up', (req,res)=>{
 
 app.post('/loginreq',(req,res)=>{
     
-       // db.close();
-    const ussr=user.findOne({'uemail':req.body.email,'upasswd':req.body.password}).toArray;
+      
+   user.findOne({'uemail':req.body.email,'upasswd':req.body.password}).exec(function (err, person) {
+       
+       
+        if (person==null) 
+        {
+           console.log(err)
+       }
+        else
+        {
+            req.session.obj =person
+            console.log(req.session.obj)
+           
+ 
+        
+           
+        return res.render('home',{username:req.session.obj.uname})
+
+        }  
+    });
     //console.log(usr.bodyParser.uname);
-    console.log(ussr)
-    if(!ussr)
-    res.send('invalid login credentials')
-    else
-    {
-     //req.session.email=req.body.email
-    console.log(ussr)
-    return res.redirect('home.html')
-    }
+   // console.log(ussr)
+  
+    
+   
 })
 
 
@@ -74,15 +89,15 @@ app.get('/',function(req,res){
     res.set({ 
         'Access-control-Allow-Origin': '*'
         }); 
-    return res.redirect('signup.html'); 
+    return res.render('login'); 
     }).listen(3000)
 
 //  Making a temporary home
 
-app.get('/home',function(req,res)
-{
-return res.render("home.html");
-});
+// app.get('/home',function(req,res)
+// {
+// return res.render("home.html");
+// });
 
 // creating a team
 
@@ -91,37 +106,43 @@ app.post('/create_team',(req,res)=>
     // const usr=user.find({'uemail':req.session})
 
     // console.log(usr);
+    if(req.session.obj.uteam!=0)
+    {
+        console.log("TEam already created")
+        return res.redirect('home')
+
+    }
     const tm=new team({
 
         tname: req.body.teamname,
         torg: req.body.organisationname,
-        tleader: req.body.leaderid,
         orgid: req.body.organisationid,
-        teamember:[req.body.leaderid],
+       
       
     })
-// There is a problem in this section
-        tm.save().then(()=>{
-            team.findByIdAndUpdate(req.body.leaderid,
-                {$push: {teamember: req.body.leaderid}},
-                {safe: true, upsert: true},
-                function(err, doc) {
-                    if(err){
-                    console.log(err);
-                    }else{
-                    //do stuff
-                    }
-                }
-            );
-        
-        
 
-        //     console.log('shgdhsdfhj');
-        //    var fn=team.findOneAndUpdate({tname:req.body.teamname},{$push:{teamember:req.body.leaderid}});
-         
-        //    //fn.save();
-        //     console.log('dhggjgddj');
-            return res.redirect('request');
+        tm.save().then(()=>{
+            
+          
+                          
+                team.findOne({'tname':req.body.teamname}).exec(function (err,teem) {
+                   console.log(req.session.obj.userId)
+                   teem.tleader=req.session.obj.userId
+                   teem.teamember.push(req.session.obj.userId)
+                   console.log(teem);
+                    teem.save();
+                    req.session.obj.uteam=teem.teamId
+                    user.findOne({'userId':req.session.obj.userId}).exec(function (err,usr) {
+                
+                       usr.uteam=teem.teamId
+                         usr.save();
+                          
+                     })
+                })
+                
+            
+            
+            return res.render('request');
         }).catch((error)=>{
               return res.send('invalid user detail or internet connection is not there');
         })
@@ -129,15 +150,75 @@ app.post('/create_team',(req,res)=>
       
 });
 
+app.get('/CreateTeamPage',function(req,res)
+{
+    res.render('CreateTeam')
+})
 // This is handle requests
 app.post('/request',(req,res)=>
 {
-   
-    return alert("Request Send Sucessfuly")
+
+    user.findOne({'userId':req.body.userid}).exec(function (err,usr) {
+             
+        usr.ureq.push(req.session.obj.userId)
+        console.log(usr)  
+        usr.save() 
+                })
+    return res.redirect('home')
 });
    
 //This is a temperary code      
-app.get('/secret',(req,res)=>
+app.get('/notifications',(req,res)=>
 {
-return res.render('secret')
+
+var request=req.session.obj.ureq[0];
+user.findOne({'userId':request}).exec(function (err,usr) {
+             
+    
+    return res.render('notifications',{req:usr.uname})
+            })
+
 });
+
+//This is submit button
+app.post('/accept',(req,res)=>
+{
+    req.session.obj.ureq;
+    console.log(req.session.obj)
+    user.findOne({'userId':req.session.obj.ureq[0]}).exec(function (err,usr) {
+             
+        team.findOne({'teamId':usr.uteam}).exec(function (err,teem) {
+             
+            teem.teamember.push(req.session.obj.userId)
+            console.log(teem)
+            teem.save()
+        
+        })
+        
+                })
+          res.render('home')   
+})
+//This is decine button
+app.post('/decline',(req,res)=>
+{
+    user.findOne({'userId':req.session.obj.userId}).exec(function (err,usr) {
+             
+    usr.ureq.pop()
+    usr.save()
+    res.render('home')
+                })
+
+
+   
+})
+
+//This is to show pages
+app.get('/homepage',function(req,res)
+{
+    res.render('home')
+})
+
+app.get('/requestpage',function(req,res)
+{
+    res.render('request')
+})
